e8fb9d4944f64ad5c71d8e3e312bbf4821a63b65
S2ej1n <ks320205@naver.com>
2024-11-29 16:35:26 +0900

    1129

---
+++ javascript/createElement/script.js
@@ -0,0 +1,10 @@
+// element 생성 , 생성할 태그의 이름 li
+const li = document.createElement('li');
+
+// 클래스 추가하기
+li.className = 'list-group-item';
+
+// id 추가하기
+li.id = 'new-item';
+
+document.querySelector()
