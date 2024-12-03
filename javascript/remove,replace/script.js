9f250f5c77c77b98159a855b13b811e5cf4916c5
S2ej1n <ks320205@naver.com>
2024-12-03 19:54:54 +0900

    1203 remove,replace

---
+++ javascript/remove,replace/script.js
@@ -0,0 +1,14 @@
+// 부모 노드
+const listParent = document.querySelector('ul');
+const list = document.querySelectorAll('li');
+
+//요소 지우기
+// listParent.removeChild(list[0]);
+
+//요소 교체하기
+const oldElement = document.getElementById('A');
+const newElement = document.createElement('span');
+
+newElement.textContent = "Hi";
+
+oldElement.parentNode.replaceChild(newElement, oldElement);
