2340fe3cec9e92a57c3dee936855afc07935e7e6
S2ej1n <ks320205@naver.com>
2024-12-03 21:32:57 +0900

    1203 이벤트리스너

--- javascript/event/script.js
+++ javascript/event/script.js
@@ -12,3 +12,22 @@ const aElement = document.querySelector('a');
 aElement.addEventListner('click', () => {
 	alert('a element clicked');
 });
+
+//
+const buttonElement = document.querySelector('.btn2');
+
+buttonElement.addEventListner('click', (event) => {
+    let val;
+    val = event.target; // 콘솔에 넣을경우 이 요소가 출력됨.
+    val = event.target.id; 
+    val = event.target.classList;
+    
+    val = event.type; //click
+
+    // 윈도우로부터의 거리 좌표
+    val = event.clientY;
+    // 요소로부터의 거리 좌표
+    val = event.offsetY;
+    
+    console.log(event);
+})
