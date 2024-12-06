53518a2993a17e0a8ccd7fc048bc3231e20af746
S2ej1n <ks320205@naver.com>
2024-12-06 18:22:29 +0900

    1206 this

--- javascript/this/script.js
+++ javascript/this/script.js
@@ -1,20 +1,39 @@
-// Method => Object
+// // Method => Object
 
-// 객체 audio
-const audio = {
-    title: 'a',
-    // 객체 안에 있으니 메소드
-    play() {
-        console.log('play this', this);
-    }
+// // 객체 audio
+// const audio = {
+//     title: 'a',
+//     // 객체 안에 있으니 메소드
+//     play() {
+//         console.log('play this', this);
+//     }
+// }
+
+// audio.play();
+
+// // audio.stop 하면 얘도 결국엔 메소드이죠.
+// // 메소드 안의 this 이므로 audio 객체를 출력하는것을 알 수 있다.
+// audio.stop = function() {
+//     console.log('stop this', this);
+// };
+
+// audio.stop();
+
+// // Function => Window Object
+// function playAudio() {
+//     console.log(this);
+// }
+
+// playAudio(); 
+
+
+// Constructor Function => {}
+function Audio(title) {
+    this.title = title;
+    console.log(this);
 }
 
-audio.play();
-
-// audio.stop 하면 얘도 결국엔 메소드이죠.
-// 메소드 안의 this 이므로 audio 객체를 출력하는것을 알 수 있다.
-audio.stop = function() {
-    console.log('stop this', this);
-};
-
-audio.stop();
+const audio = new Audio('a');
+// 빈 객체 안에 타이틀은 a
+// 그래서 Audio {title: 'a}가 출력됨.
+// this.title = title; 얘가 없으면 빈 객체 반환
