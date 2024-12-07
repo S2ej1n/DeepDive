b691cb964e765e21d59f2a4de31d71439e3cb83e
S2ej1n <ks320205@naver.com>
2024-12-07 21:09:52 +0900

    1206 apply bind

--- javascript/call,bind,apply/script.js
+++ javascript/call,bind,apply/script.js
@@ -1,11 +1,43 @@
-// Call();
-const fullName = function(city, country) {
-    console.log(this.firstName + '' + this.lastName, city, country);
+// // Call();
+// const fullName = function(city, country) {
+//     console.log(this.firstName + ' ' + this.lastName, city, country);
+// }
+
+// const person1 = {
+//     firstName: 'John',
+//     lastName: 'Smith'
+// }
+
+// fullName.call(person1, "Oslo", "Norway");
+
+// // Apply();
+// const fullName = function(city, country) {
+//     console.log(this.firstName + ' ' + this.lastName, city, country);
+// }
+
+// const person1 = {
+//     firstName: 'John',
+//     lastName: 'Smith'
+// }
+
+// fullName.apply(person1, ["Oslo", "Norway"])
+// // 다른건 인수를 넣어줄 때 배열로 넣어주면 된다.
+
+// bind()
+function func(language) {
+    if (language === "kor") {
+        console.log(`language: ${this.korGreetung}`);
+    } else {
+        console.log(`language: ${this.engGreetung}`);
+    }
 }
 
-const person1 = {
-    firstName: 'John',
-    lastName: 'Smith'
+const greeting = {
+    korGreetung: '안녕',
+    engGreetung: 'Hello',
 }
 
-fullName.call(person1, "Oslo", "Norway");
+const boundFunc = func.bind(greeting)('eng');
+// this에대 이 객체를 바인딩만 시켜주는것
+// 변수에다 함수 할당하고, 재 호출해줘야함
+// boundFunc("kor");
