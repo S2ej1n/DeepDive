2a45c186a87e8b7426fe3b9e46336308a003ba90
S2ej1n <ks320205@naver.com>
2024-12-07 23:15:47 +0900

    1207 closure

--- javascript/closuer/script.js
+++ javascript/closuer/script.js
@@ -1,11 +1,27 @@
-// What is closure?
-function outerFunction(outerVariable) {
-    return function innerFunction(innerVariable) {
-        console.log('Outer Variable: ' + outerVariable);
-        console.log('Inner Variable: ' + innerVariable);
-    }
+// // What is closure?
+// function outerFunction(outerVariable) {
+//     return function innerFunction(innerVariable) {
+//         console.log('Outer Variable: ' + outerVariable);
+//         console.log('Inner Variable: ' + innerVariable);
+//     }
+// }
+
+// const newFunction = outerFunction('outside');
+// // console.log('New Function: ' + newFunction);
+// newFunction('inside');
+
+let a = 'a';
+
+function functionB() {
+    let a = 'c';
+    console.log(a,b,c); // c is accessible here
 }
 
-const newFunction = outerFunction('outside');
-console.log('New Function: ' + newFunction);
-// newFunction('inside');
+function functionA() {
+    let b = 'b';
+    functionB()
+    console.log(a, b); // a is accessible here
+}
+functionA();
+
+// 이걸 closure로 해결할수있다.
