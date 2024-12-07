04ca32cdb5ab22e79fe817f492d509f975691bd6
S2ej1n <ks320205@naver.com>
2024-12-08 03:41:57 +0900

    1208 IIFE

--- javascript/IIFE/script.js
+++ javascript/IIFE/script.js
@@ -5,9 +5,17 @@
 //     }
 // )()
 
-(function () {
-    var aName = "Barry";
+// (function () {
+//     var aName = "Barry";
+// })();
+// // IIFE 내부에서 정의된 변수는 외부 범위에서 접근이 불가능하다.
+// console.log(aName)
+// // throws "Uncaught ReferenceError: aName is not defined"
+
+var result = (function () {
+    var name = "Barry";
+    return name;
 })();
-// IIFE 내부에서 정의된 변수는 외부 범위에서 접근이 불가능하다.
-console.log(aName)
-// throws "Uncaught ReferenceError: aName is not defined"
+// 함수 자체가 리턴이 되는게 아니라, 결과값이 바로 리턴이됨.
+console.log(result);
+// "Barry"
