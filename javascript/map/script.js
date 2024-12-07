e203012530e74e7da5006103a03c42ba23fc34af
S2ej1n <ks320205@naver.com>
2024-12-08 01:48:52 +0900

    1208 reducer

--- javascript/map/script.js
+++ javascript/map/script.js
@@ -1,8 +1,16 @@
-const array1 = [1, 4, 9, 16];
+// const array1 = [1, 4, 9, 16];
 
-const map1 = array1.map(function (item, index, array) {
-    console.log(item, index, array, this);
-    return (item * 2);
-}, { a: 'a'});
+// const map1 = array1.map(function (item, index, array) {
+//     console.log(item, index, array, this);
+//     return (item * 2);
+// }, { a: 'a'});
 
-console.log(map1);
+// console.log(map1);
+
+const array = [1, 2, 3, 4];
+
+const sum = array.reduce((accumulator, currentValue, currentIndex, array) => {
+    return accumulator + currentValue;
+});
+
+console.log(sum); // 10
