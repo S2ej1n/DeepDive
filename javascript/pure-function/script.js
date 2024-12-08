4619af496cec9f5ee6f18738361ce8cb6df57efb
S2ej1n <ks320205@naver.com>
2024-12-08 11:49:42 +0900

    1208 퓨어함수(2)

--- javascript/pure-function/script.js
+++ javascript/pure-function/script.js
@@ -11,10 +11,16 @@
 // console.log(x); // 0
 
 const alphabetArray = ['A', 'B'];
-const addItemToArray = (originalArray, newItem) => {
-    originalArray.push(newItem);
-    return originalArray;
+// const addItemToArray = (originalArray, newItem) => {
+//     originalArray.push(newItem);
+//     return originalArray;
+// }
+
+const pureAddItemToArray = (originalArray, newItem) => {
+    return [...originalArray, newItem];
+    //  얕은 복사 해버림
 }
 
-console.log(addItemToArray(alphabetArray, 'C'));
+console.log(pureAddItemToArray(alphabetArray, 'C'));
 console.log(alphabetArray);
+// 함수 외부값에는 변화주지 않음.
