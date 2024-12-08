44eaf1f2ae18dea32ba48293c5ee620402a25301
S2ej1n <ks320205@naver.com>
2024-12-08 12:13:15 +0900

    1208 커링함수 이렇게

--- javascript/currying-function/script.js
+++ javascript/currying-function/script.js
@@ -28,24 +28,43 @@
 
 // ----------------------------------------------------------------
 
-function log(date, importance, message) {
-    alert(`[${date.getHours()}:${date.getMinutes()}] [${importance}] ${message}`);
-}
+// function log(date, importance, message) {
+//     alert(`[${date.getHours()}:${date.getMinutes()}] [${importance}] ${message}`);
+// }
 
-// log(new Date(), "DEBUG", "some debug");
+// // log(new Date(), "DEBUG", "some debug");
 
-// 위의꺼를 커링으로 바쭤주기 위해서 curry함수를 사용할수있다.
-function curry(f) { 
-    return function (a) {
-        return function (b) {
-            return function (c) {
-                return f(a, b, c);
+// // 위의꺼를 커링으로 바쭤주기 위해서 curry함수를 사용할수있다.
+// function curry(f) { 
+//     return function (a) {
+//         return function (b) {
+//             return function (c) {
+//                 return f(a, b, c);
+//             }
+//         };
+//     };
+// }
+
+// const curriedLog = curry(log);
+// curriedLog(new Date())("DEBUG")("some debug");
+
+// 근데 위의 저 방식을 사용하면 인수가 추가될때 마다 계속 리턴값 추가추가해줘야됨.
+// 그래서 이런 문제 해결하려고 ‘다이나믹하게 쿼리함수’ 만들어서 쓸 수 있음.
+
+// 이거 쓰면 매개변수가 몇개이든 쿼리 함수로 바꿔줄 수 있다.
+function curry(func) {
+    return function curried(...args) {
+        if (args.length >= func.length) {
+            return func.apply(this, args);
+        } else {
+            return function (...args2) {
+                return curried.apply(this, args.concat(args2));
             }
-        };
+        }
     };
 }
 
-const curriedLog = curry(log);
-curriedLog(new Date())("DEBUG")("some debug");
+const sum = (x, y, z, j, a) => x + y + z + j + a;
 
-// 근데 위의 저 방식을 사용하면 인수가 추가될때 마다 계속 리턴값 추가추가해줘야됨.
+const curriedSum = curry(sum);
+console.log(curriedSum(1)(2)(3)(4)(5)); // 15
