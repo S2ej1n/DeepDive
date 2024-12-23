751a1204d60dcc2988bba15bd1756122da024e46
S2ej1n <ks320205@naver.com>
2024-12-23 17:19:01 +0900

    strict-mode

--- javascript/strict-mode/script.js
+++ javascript/strict-mode/script.js
@@ -1,14 +1,27 @@
-"use strict"
-// 전체에다 적용
+// "use strict"
+// // 전체에다 적용
 
-// 모두 말고 함수에다가 적용하려면
-function sum(a,b){
-    "use strict"
-    return a + b;
-}
-// 이러면 함수에만 엄격한 모드
+// // 모두 말고 함수에다가 적용하려면
+// function sum(a,b){
+//     "use strict"
+//     return a + b;
+// }
+// // 이러면 함수에만 엄격한 모드
 
-// 마지막 방법
-class Cat {
-    
-}
+// // 마지막 방법
+// class Cat {
+
+// }
+
+'use strict'
+undefined = 8
+NaN = 10;
+
+console.log(undefined);
+console.log(NaN);
+
+let greeting = "Hello";
+
+greating = "Hi";
+
+console.log(greeting); 
