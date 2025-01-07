26d9c2b2d95e23712cb86d5ec17c00e148296af5
S2ej1n <ks320205@naver.com>
2025-01-07 16:29:54 +0900

    프로토타입-Object.create

--- javascript/protorype/script.js
+++ javascript/protorype/script.js
@@ -1,19 +1,37 @@
+// function Person(name, email, birthday) {
+//     this.name = name;
+//     this.email = email;
+//     this.birthday = new Date(birthday);
+//     // this.calculateAge = function() {
+//     //     const diff = Date.new() - this.birthday.getTime();
+//     //     const ageDate = new Date(diff);
+//     //     return Math.abs(ageDate.getUTCFullYear() - 1970);
+//     // }
+// }
+
+// // 프로토타입에다 넣어주는것. => 함수는 재사용이 가능하니까.
+// Person.prototype.calculateAge = function() {
+//     const diff = Date.new() - this.birthday.getTime();
+//     const ageDate = new Date(diff);
+//     return Math.abs(ageDate.getUTCFullYear() - 1970);
+// }
+
+
 function Person(name, email, birthday) {
-    this.name = name;
-    this.email = email;
-    this.birthday = new Date(birthday);
-    // this.calculateAge = function() {
-    //     const diff = Date.new() - this.birthday.getTime();
-    //     const ageDate = new Date(diff);
-    //     return Math.abs(ageDate.getUTCFullYear() - 1970);
-    // }
+    // person이라는 새로운 객체를 만들게 됨
+    const pereson = Object.create(PersonsPrototype);
+    pereson.name = name;
+    pereson.email = email;
+    pereson.birthday = new Date(birthday);
+    return pereson;
 }
 
-// 프로토타입에다 넣어주는것. => 함수는 재사용이 가능하니까.
-Person.prototype.calculateAge = function() {
-    const diff = Date.new() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
+const PersonsPrototype = {
+    calculateAge() {
+        const diff = Date.new() - this.birthday.getTime();
+        const ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
 }
 
 // new 키워드
