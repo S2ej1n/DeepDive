75ee35a55912ce1e0985c1c4ced6ea4ce6333607
S2ej1n <ks320205@naver.com>
2025-01-08 15:50:40 +0900

    Asynsc

--- javascript/0108/async/script.js
+++ javascript/0108/async/script.js
@@ -1,34 +1,13 @@
-// // 얘가 리턴하는건 Promise 객체
-// function fetchData() {
-//     return new Promise ((resolve, reject) => {
-//         const success = true;
-//         if (success) {
-//             resolve('성공');
-//         } else {
-//             reject('실패');
-//         }
-//     })
-// }
-
-// fetchData()
-//     .then((response)=>{
-//         console.log(response);
-//     })
-//     .catch((error)=>{
-//         console.error(error);
-//     })
-
-// 얘는 promise를 지원함.
-fetch('http://jsonplaceholder.typicode.com/todos/1')
-    .then(response1 => response1.json())
-    .then(json1 => console.log(json1))
-    .then(() => fetch('http://jsonplaceholder.typicode.com/todos/2'))
-    .then(response2 => response2.json())
-    .then(json2 => console.log(json2))
-    .catch((error) => {
+async function makeRequest() {
+    try {
+        const response1 = await fetch('http://jsonplaceholder.typicode.com/todos/1')
+        const jsonResponse1 = await response1.json();
+        console.log(jsonResponse1);
+    } catch (error) {
         console.error(error);
-    })
-    // 성공을 하든 실패를 하든 항상 파이널리는 지나감
-    .finally(()=>{
-        console.log('작업 끝!')
-    })
+    } finally {
+        console.log('모든작업끝')
+    }
+}
+
+makeRequest();
