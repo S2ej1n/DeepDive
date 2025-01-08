d1fcd7aa6a8664baa6d03a87262505842382dfa8
S2ej1n <ks320205@naver.com>
2025-01-08 13:01:18 +0900

    class extends

--- javascript/0107/classes/script.js
+++ javascript/0107/classes/script.js
@@ -1,26 +1,49 @@
+// class Person {
+//     constructor(name, email, birthday){
+//         this.name = name;
+//         this.email = email;
+//         this.birthday = new Date(birthday);
+//     }
+
+//     // 클래스 문법 사용 - 함수 자체가 프로토타입에 들어감.
+//     // 프로토타입에 인트로듀스가 들어감.
+//     introduce() {
+//         return `Hello my name is ${this.name}`;
+//     }
+
+//     static multipleNumbers(x,y) {
+//         return x * y;
+//     }
+// }
+
+// const john = new Person('john', 'john@example.com','10-3-98');
+// console.log(john);
+
+// // introduce사용하려면
+// john.introduce();
+
+// // 클래스 자체에 있는 멀티플넘버스를 사용하려면.
+// Person.multipleNumbers(1,2)
+
 class Person {
-    constructor(name, email, birthday){
+    constructor(name, email) {
         this.name = name;
         this.email = email;
-        this.birthday = new Date(birthday);
     }
 
-    // 클래스 문법 사용 - 함수 자체가 프로토타입에 들어감.
-    // 프로토타입에 인트로듀스가 들어감.
     introduce() {
         return `Hello my name is ${this.name}`;
     }
+}
 
-    static multipleNumbers(x,y) {
-        return x * y;
+class Client extends Person {
+    constructor(name, email, phone, address) {
+        super(name, email)
+
+        this.phone = phone;
+        this.address = address;
     }
 }
 
-const john = new Person('john', 'john@example.com','10-3-98');
-console.log(john);
-
-// introduce사용하려면
-john.introduce();
-
-// 클래스 자체에 있는 멀티플넘버스를 사용하려면.
-Person.multipleNumbers(1,2)
+const john = new Client('John', 'john@example.com', '010-1234-5678', 'Seoul');
+console.log(john.introduce());
