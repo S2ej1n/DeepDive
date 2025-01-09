9046865e0646585f8b47c004a6464fb314c665b2
S2ej1n <ks320205@naver.com>
2025-01-09 10:02:36 +0900

    Iterator

--- javascript/0108_2/iterator,generator/script.js
+++ javascript/0108_2/iterator,generator/script.js
@@ -13,7 +13,10 @@ function makeIterator(numbers) {
 // 숫자 배열 생성
 const numbersArray = [1,2,3];
 
-const numbersIterator = makeIterator(numbersArray);
+// const numbersIterator = makeIterator(numbersArray);
+
+// 이렇게도 할 수 있다.
+const numbersIterator = numbersArray[Symbol.iterator]()
 
 console.log(numbersIterator.next());
 console.log(numbersIterator.next());
