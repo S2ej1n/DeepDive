87a1981855363ff570c145df839a83197a827a94
S2ej1n <ks320205@naver.com>
2025-01-09 10:09:18 +0900

    Iterator

--- javascript/0108_2/iterator,generator/script.js
+++ javascript/0108_2/iterator,generator/script.js
@@ -11,14 +11,22 @@ function makeIterator(numbers) {
 }
 
 // 숫자 배열 생성
-const numbersArray = [1,2,3];
+// const numbersArray = [1,2,3];
 
-// const numbersIterator = makeIterator(numbersArray);
+// // const numbersIterator = makeIterator(numbersArray);
 
-// 이렇게도 할 수 있다.
-const numbersIterator = numbersArray[Symbol.iterator]()
+// // 이렇게도 할 수 있다.
+// const numbersIterator = numbersArray[Symbol.iterator]()
 
-console.log(numbersIterator.next());
-console.log(numbersIterator.next());
-console.log(numbersIterator.next());
-console.log(numbersIterator.next());
+// console.log(numbersIterator.next());
+// console.log(numbersIterator.next());
+// console.log(numbersIterator.next());
+// console.log(numbersIterator.next());
+
+const numbersIterable = [1,2,3];
+const numbersNotIterable = { a:1, b:2 };
+console.log(typeof numbersIterable);
+console.log(typeof numbersNotIterable);
+for ( const n of numbersNotIterable ){
+    console.log(n);
+}
