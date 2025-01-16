3a8d064241803b4e269b5d0665ff724c30e49295
S2ej1n <ks320205@naver.com>
2025-01-17 01:15:23 +0900

    스프레드시트(4)

--- js-project/SpreadSheet/script.js
+++ js-project/SpreadSheet/script.js
@@ -5,12 +5,14 @@ const spreadsheet = []
 const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 class Cell {
-    constructor(isHeader, disabled, data, row, column, active = false){
+    constructor(isHeader, disabled, data, row, column, rowName, coulumName, active = false){
         this.isHeader = isHeader;
         this.disabled = disabled;
         this.data = data;
         this.row = row;
         this.column = column;
+        this.rowName = rowName;
+        this.coulumName = coulumName;
         this.active = active;
     }
 }
@@ -22,14 +24,20 @@ function initSpreadSheet() {
         let spreadsheetRow = [];
         for (let j = 0; j < COLS; j++) {
             let cellData = "";
+            let isHeader = false;
+            let disabled = false;
 
             // 모든 row 첫 번째 칼럼에 숫자 넣기
             if (j === 0) {
                 cellData = i;
+                isHeader = true;
+                disabled = true;
             }
 
             if (i === 0){
+                isHeader = true;
                 cellData = alphabet[j - 1];
+                disabled = true;
             }
 
             // 모든 첫 번째 row의 컬럼은 ''
@@ -37,7 +45,10 @@ function initSpreadSheet() {
                 cellData = '';
             }
 
-            const cell = new Cell(false, false, cellData, i, j, false);
+            const rowName = i;
+            const columnName = alphabet[j -1];
+
+            const cell = new Cell(isHeader, disabled, cellData, i, j, rowName, columnName, false);
             spreadsheetRow.push(cell);
         }
         spreadsheet.push(spreadsheetRow);
@@ -49,10 +60,33 @@ function initSpreadSheet() {
 function createCellEl(cell){
     const cellEl = document.createElement('input');
     cellEl.className = 'cell';
-    cellEl.id = 'celll_' + cell.row + cell.column;
+    cellEl.id = 'cell_' + cell.row + cell.column;
     cellEl.value = cell.data;
     cellEl.disabled = cell.disabled;
+
+    if (cell.isHeader){
+        cellEl.classList.add('header');
+    }
+
+    cellEl.onclick = () => handleCellClick(cell);
+
     return cellEl;
+}
+
+function handleCellClick(cell){
+    const columnHeader = spreadsheet[0][cell.column];
+    const rowHeader = spreadsheet[cell.row][0];
+
+    const columnHeaderEl = getElFromRowCol(columnHeader.row, columnHeader.column);
+    const rowHeaderEl = getElFromRowCol(rowHeader.row, rowHeader.column);
+    columnHeaderEl.classList.add("active");
+    rowHeaderEl.classList.add("active");
+
+    console.log('clicked cell', columnHeaderEl, rowHeaderEl);
+}
+
+function getElFromRowCol(row, col){
+    return document.querySelector("#cell_" + row + col);
 }
 
 function drawSheet() {
